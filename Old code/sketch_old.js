3ad399ab8afdb579eb8c86f3aa7ebe79ad306216
DinaMostafa00 <modi21tf@student.ju.se>
2024-01-19 16:04:09 +0100

    new

---
+++ Old code/sketch_old.js
@@ -0,0 +1,25 @@
+let rot = 0;
+let rotSpeed = 0.1;
+
+function preload() {
+  createVRCanvas();
+}
+
+function setup() {
+  setVRBackgroundColor(255);
+}
+
+function calculate() {}
+
+function draw() {
+  rot += rotSpeed;
+  rotateY(rot);
+  fill(100, 240, 100);
+  box(20);
+  translate(-20, 0, 0);
+  fill(240, 100, 100);
+  box(20);
+  translate(40, 0, 0);
+  fill(100, 100, 240);
+  box(20);
+}
