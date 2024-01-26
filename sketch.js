bd6d2c78fad42a41a0895cc904d72e41ca9c187e
DinaMostafa00 <modi21tf@student.ju.se>
2024-01-26 18:02:48 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -236,39 +236,48 @@ function preload() {
 
 function setup() {
   setVRBackgroundColor(100, 100, 100);
+  for (let i = 0; i < num; i++) {
+    particles.push(
+      createVector(random(width), random(height), random(-700, 700))
+    );
+  }
 }
 
 function draw() {
-  translate(0, 0, -700);
+  translate(width / 2, height / 2, -700);
   drawFlowField();
 }
 
 function drawFlowField() {
-  for (let i = 0; i < num; i++) {
-    particles.push(createVector(random(width), random(height)));
-  }
-
   stroke(random(200, 255), random(0, 20), random(20, 50));
-
   background(0, 10);
   for (let i = 0; i < num; i++) {
     let p = particles[i];
-    point(p.x, p.y);
-    let n = noise(
-      p.x * noiseScale,
-      p.y * noiseScale,
-      frameCount * noiseScale * noiseScale
-    );
+    point(p.x, p.y, p.z);
+    let n = noise(p.x * noiseScale, p.y * noiseScale, p.z * noiseScale);
     let a = TAU * n + flowDirection;
     p.x += cos(a) * stepSize;
     p.y += sin(a) * stepSize;
+    p.z +=
+      (noise(p.x * noiseScale, p.y * noiseScale, frameCount * noiseScale) -
+        0.5) *
+      stepSize *
+      2;
     if (!onScreen(p)) {
       p.x = random(width);
       p.y = random(height);
+      p.z = random(-700, 700);
     }
   }
 }
 
 function onScreen(v) {
-  return v.x >= 0 && v.x <= width && v.y >= 0 && v.y <= height;
+  return (
+    v.x >= 0 &&
+    v.x <= width &&
+    v.y >= 0 &&
+    v.y <= height &&
+    v.z >= -700 &&
+    v.z <= 700
+  );
 }
